400a850c43c0e9c4b00c77b8ca52ece636d84501
Soushi888 <sacha.pignot@protonmail.com>
2024-02-25 23:58:37 -0500

    cors policy issue with coingecko API

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -3,17 +3,15 @@ import { coinDataStore, coinsListStore } from '@stores/coins.store';
 interface IApiService {
 	getCoinsMarketData(): Promise<any>;
 	getCoinData(coinId: string): Promise<any>;
-	// Add other methods as needed
 }
 
 export default class ApiService implements IApiService {
 	private readonly baseUrl = 'https://api.coingecko.com/api/v3';
-	private readonly apiKey = import.meta.env.VITE_COINGECKO_API_KEY;
-	private readonly requestInit = {
+	private readonly requestInit: RequestInit = {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*'
+			mode: 'no-cors'
 		}
 	};
 	private fetch: typeof fetch;
@@ -28,23 +26,27 @@ export default class ApiService implements IApiService {
 	}
 
 	async getCoinData(coinId: string): Promise<void> {
-		const response = await this.fetch(`${this.baseUrl}/coins/${coinId}`, this.requestInit);
-		if (!response.ok) {
-			throw new Error('Failed to fetch coin data');
-		}
-		const data = await response.json();
-		coinDataStore.set(data);
+		try {
+			const response = await this.fetch(`${this.baseUrl}/coins/${coinId}`, this.requestInit);
+			if (!response.ok) {
+				throw new Error('Failed to fetch coin data');
+			}
+			const data = await response.json();
+			coinDataStore.set(data);
+		} catch (e) {}
 	}
 
 	async getCoinsMarketData(): Promise<void> {
-		const response = await this.fetch(
-			`${this.baseUrl}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false?x_cg_pro_api_key=${this.apiKey}`,
-			this.requestInit
-		);
-		if (!response.ok) {
-			throw new Error('Failed to fetch coin list');
-		}
-		const data = await response.json();
-		coinsListStore.set(data);
+		try {
+			const response = await this.fetch(
+				`${this.baseUrl}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`,
+				this.requestInit
+			);
+			if (!response.ok) {
+				throw new Error('Failed to fetch coin list');
+			}
+			const data = await response.json();
+			coinsListStore.set(data);
+		} catch (e) {}
 	}
 }
